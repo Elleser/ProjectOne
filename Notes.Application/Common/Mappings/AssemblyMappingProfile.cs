0ea3e4e06328f64169115de4a819640808f97572
Elleser <112511959+Elleser@users.noreply.github.com>
2022-09-03 21:29:24 +0300

    !!!

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+
+namespace Notes.Application.Common.Mappings
+{
+    internal class AssemblyMappingProfile : Profile
+    {
+
+        AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingFromAssembly(assembly);
+
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+
+    }
+}
